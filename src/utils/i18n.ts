1b24498610c44faf2c66cc67a4cc23e883cb6ed1
Mauricio F <mauriciofsnts@gmail.com>
2022-12-23 20:16:07 -0300

    fix: i18n not working in production mode

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,12 +1,16 @@
 import i18n from 'i18n'
 import { join } from 'path'
 
+import en from '../locales/en.json'
+import ptBr from '../locales/pt_br.json'
+
 i18n.configure({
   locales: ['pt_br', 'en'],
-  directory: join(__dirname, '..', 'locales'),
+  directory: join(__dirname, "..", "locales"),
   defaultLocale: 'en',
   retryInDefaultLocale: true,
   objectNotation: true,
+  updateFiles: true,
 
   logWarnFn: function (msg: string) {
     console.log(msg)
