c111fbefa6f6d3581e9e375dd63886ffe7f931b1
Mauricio F <mauriciofsnts@gmail.com>
2023-02-13 13:28:37 -0300

    feat(play): enhance embed

--- src/cron/news.ts
+++ src/cron/news.ts
@@ -12,6 +12,8 @@ import cron from 'node-cron'
 //                       # │ │ │ │ │ │
 //                       # * * * * * *
 const job = cron.schedule('0 0 * * *', () => {
+  console.info('Daily news request reset')
+  
   // reset daily news request
   client.dailyNewsRequest = 0
 })
