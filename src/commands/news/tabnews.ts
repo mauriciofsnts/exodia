ca443d3df02b29d18667904cd2131a7b4a0aba2e
Mauricio F <mauriciofsnts@gmail.com>
2023-04-07 13:00:48 -0300

    feat: improving the presentation of tabnews command

--- src/commands/news/tabnews.ts
+++ src/commands/news/tabnews.ts
@@ -1,37 +1,38 @@
-import { Embed, Reply } from 'commands/reply'
-import { Command } from 'core/command'
-import { i18n } from 'utils/i18n'
-import { getTabNews } from 'core/tabnews'
-import { UrlShortener } from 'core/URLShortener'
+import { Embed, Reply } from 'commands/reply';
+import { Command } from 'core/command';
+import { i18n } from 'utils/i18n';
+import { getTabNews } from 'core/tabnews';
+import { UrlShortener } from 'core/URLShortener';
 
 export default new Command({
-  name: 'tabnews',
-  description: i18n.__('tabnews.description'),
-  categorie: '📰 News',
-  aliases: ['tabnews'],
-  run: async ({ interaction, type }) => {
-    
-    getTabNews().then(async (news) => {
-      const articles = news.getAll()
-      const shortener = new UrlShortener()
+	name: 'tabnews',
+	description: i18n.__('tabnews.description'),
+	categorie: '📰 News',
+	aliases: ['tabnews'],
+	run: async ({ interaction, type }) => {
+		getTabNews().then(async (news) => {
+			const articles = news.getAll();
+			const shortener = new UrlShortener();
 
-      const embed = Embed({
-        title: i18n.__('news.result'),
-        type: 'success',
-      })
+			const embed = Embed({
+				title: i18n.__('news.result'),
+				type: 'success',
+			});
 
-      for (const article of articles) {
-        const shortUrl = await shortener.shorten(
-          `https://www.tabnews.com.br/${article.owner_username}/${article.slug}`
-        )
+			for (const article of articles) {
+				const shortUrl = await shortener.shorten(
+					`https://www.tabnews.com.br/${article.owner_username}/${article.slug}`
+				);
 
-        embed.addFields({
-          name: article.title,
-          value: shortUrl,
-        })
-      }
+				embed.addFields({
+					name: article.title,
+					value: `⭐ ${article.tabcoins} · ${article.owner_username} · ${shortUrl}`,
+				});
+			}
 
-      Reply(embed, interaction, type)
-    })
-  },
-})
+			embed.setURL('https://www.tabnews.com.br');
+			embed.setTimestamp()
+			Reply(embed, interaction, type);
+		});
+	},
+});
